919941a9ce8226d49732fa390a3cd825452eab95
ambermhh <ambermhh@hotmail.com>
2023-04-14 17:04:11 +1000

    000

---
+++ wastedNot/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css'
+
+function HomePage (){
+
+    return (
+        <div className="home-page">
+            <h1 className="brand_name">WASTED NOT</h1>
+
+        </div>
+    )
+}
+
+export default HomePage
